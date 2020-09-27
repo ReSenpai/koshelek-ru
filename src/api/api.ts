24c0b93afbdda0be7beec7ab90885b3717115cbc
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-27 23:12:24 +0300

    Add getDepth and streamDepth

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,43 @@
 import axios from 'axios';
+import { DepthType } from '../types/types';
 
 const binance = axios.create({
-    // withCredentials: true,
-    baseURL: 'https://api.binance.com',
-    // headers: {
-    //     apiKey: 'lMVaebhHxqx5vB9XJN0eIeUQUj5ceCUAu4yefQOyqDiafCulqChseOjZLO4iheof',
-    //     secretKey: 'IDrxYp43fDC6BqxxsDDn84pYyNZLsfhqEOBkPwObSFzOuNpFURiVITvhPpt2zCuZ',
-    //     "proxy": "http://localhost:3000",
-    // }
+    baseURL: 'https://api.binance.com'
 });
 
 export const binanceAPI = {
-    ping () {
-        return binance.get(`/api/v3/depth`)
+    getDepth (symbol: string = 'BTCUSDT') {
+        return binance.get<DepthType>(`/api/v3/depth?limit=500&symbol=${symbol}`)
         .then(response => response.data);
+    },
+    async streamDepth (symbol: string = 'BTCUSDT') {
+        const ws = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol}@depth@1000ms`);
+        const config = {
+            "method": "SUBSCRIBE",
+            "params": [
+              `${symbol.toLowerCase()}@depth`
+            ],
+            "id": 1
+        }
+
+        ws.onopen  = function(e) {
+            console.log("[open] Соединение установлено");
+            console.log("Отправляем данные на сервер");
+            ws.send(JSON.stringify(config));
+        }
+        
+        ws.onclose = function(event) {
+            if (event.wasClean) {
+                console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+            } else {
+                console.log('[close] Соединение прервано');
+            }
+        };
+        
+        ws.onerror = function(error: any) {
+            console.log(`[error] ${error.message}`);
+        };
+
+        return ws;
     }
 }
