dddba58b990c64d307c3c121dd6c78415772157a
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-27 23:14:20 +0300

    Update TableContainer.tsx

--- src/components/Table/TableContainer.tsx
+++ src/components/Table/TableContainer.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import Table from './Table';
+import { connect } from 'react-redux';
+import { AppStateType } from '../../redux/store';
+import { TableType } from '../../types/types';
+import Tables from './Table';
+import { getAsks, getBids } from '../../redux/exchange_selectors';
 
-const TableContainer = () => {
+type MapStatePropsType = {
+    asks: Array<TableType>
+    bids: Array<TableType>
+}
+type MapDispatchPropsType = {
+    
+}
+type OwnPropsType = {
+}
+export type PropsType = MapStatePropsType & MapDispatchPropsType & OwnPropsType;
+
+const TableContainer: React.FC<PropsType> = (props) => {
     return (
-        <Table />
+        <Tables {...props} />
     );
 }
 
-export default TableContainer;
+const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
+    asks: getAsks(state),
+    bids: getBids(state)
+}) 
+
+
+export default connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, AppStateType>(mapStateToProps, {
+    
+})(TableContainer);
