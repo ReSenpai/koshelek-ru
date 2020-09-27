6ae1a4c502391aebde985541bf3bcce21ae2e947
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-27 23:13:07 +0300

    Update exchange_selectors.ts

--- src/redux/exchange_selectors.ts
+++ src/redux/exchange_selectors.ts
@@ -1,3 +1,5 @@
 import { AppStateType } from "./store";
 
 export const getInitially = (state: AppStateType) => state.exchangePage.initially;
+export const getBids = (state: AppStateType) => state.exchangePage.bids;
+export const getAsks = (state: AppStateType) => state.exchangePage.asks;
